316b664f8cffdfd8f0ff39e46a5ac318488b64c7
Jason Cable <cablej02@gmail.com>
2025-02-27 13:49:32 -0600

    added basic navbar no styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,30 @@
-import { Link } from 'react-router-dom';
-import Auth from '../utils/auth';
+import { useUser } from '../context/UserContext';
+import AuthService from '../utils/auth';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
-  return (
-    <>
+    const { user } = useUser();
+    const navigate = useNavigate();
 
-    </>
-  );
+    const handleLogout = () => {
+        AuthService.logout(navigate);
+    };
+
+    return (
+        <nav>
+            <h1>Cellar</h1>
+            <div>
+                {user ? (
+                    <>
+                        <p>{user.username}</p>
+                        <button onClick={handleLogout}>Logout</button>
+                    </>
+                ) : (
+                    <span>Loading...</span>
+                )}
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
