5652c2a6e62f65487c5e029ab2f358af3c8c8f86
Jason Cable <cablej02@gmail.com>
2025-02-28 23:21:45 -0600

    updated capitalizeWords to not capitalize an apostraphe+s

--- client/src/utils/formatting.js
+++ client/src/utils/formatting.js
@@ -1,3 +1,7 @@
 export const capitalizeWords = (str) => {
-    return str ? str.replace(/\b\w/g, (char) => char.toUpperCase()) : '';
+    return str
+        ? str.replace(/\b\w/g, (char, index, text) =>
+            index === 0 || text[index - 1] !== "'" ? char.toUpperCase() : char
+        )
+        : '';
 };
